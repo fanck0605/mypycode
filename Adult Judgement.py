61d821b2ba822e9aa466a07b2d784c25e00db42e
fanck0605 <fanck0605@qq.com>
2018-07-03 19:26:12 +0800

    update

--- Adult Judgement.py
+++ Adult Judgement.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
 age = int(input('Please Enter Your Age: '))
-if age >= 18:
-    print('Hello, Adult!')
+
+if 0 <= age <=100:
+    if age >= 18:
+        print('Hello, Adult!')
+    elif age >= 6:
+        print('Hello, Teenager!')
+    else:
+        print('Hello, Kid!')
 else:
-    print('Hello, Teenager!')
+    print('Please Enter Your Correct Age!')
---
+++ Somatotype Test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+print('欢迎测试！\n')
+
+height = float(input('请输入您的身高(m): '))
+weight = float(input('请输入您的体重(kg): '))
+
+bmi = weight/(height*height)
+print('\nBMI: %.1f' % bmi)
+if bmi > 32:
+    print('严重肥胖')
+elif bmi > 28:
+    print('肥胖')
+elif bmi > 25:
+    print('过重')
+elif bmi > 18.5:
+    print('正常')
+else:
+    print('过轻')

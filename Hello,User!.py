920ef571e55ee53d9a840ab9e471b1f5180cf111
fanck0605 <fanck0605@qq.com>
2018-07-04 13:35:49 +0800

    remove bom

--- Hello,User!.py
+++ Hello,User!.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 name = input('Please Enter Your Name: ')
 print('Hello,', name,'!')
--- Hello,World!.py
+++ Hello,World!.py
@@ -1,3 +1,3 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 print('Hello!\nThe World!')
